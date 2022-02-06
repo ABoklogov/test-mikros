8ce1fa664ee1b964da713dfb755c828f320fc56b
Alexey Boklogov <kashira87@mail.ru>
2022-02-06 13:45:01 +0300

    adds vars.css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/fonts.css';
+import './css/vars.css';
 import './css/index.css';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
